58ea6f130300b5f8e2408d9457ee15311f381a68
alex tert <alex@iteamo.net>
2019-04-06 23:38:25 +0700

    Add files via upload

---
+++ app.php
@@ -0,0 +1,24 @@
+<?
+// ALEX.ITEAMO.NET <<< =========================================================
+// Alex Tert. Web-development. CRM, ERP, VOIP, LMS, cloud services integration
+// Web: alex.iTeamo.net  Skype: alexITmore  ICQ: 235004598  Mail: alex@iteamo.net
+// =============================================================================
+namespace iteamo;
+// =============================================================================
+require_once(__dir__ . '/framework/app.php');
+require_once(__dir__ . '/framework/traits/withSingleton.php');
+require_once(__dir__ . '/framework/traits/withInheritance.php');
+require_once(__dir__ . '/framework/traits/withServices.php');
+// =============================================================================
+/**
+ *
+ */   
+class app extends framework\app {
+// =============================================================================
+use \iteamo\framework\traits\withSingleton;
+use \iteamo\framework\traits\withInheritance;
+use \iteamo\framework\traits\withServices;
+// -----------------------------------------------------------------------------
+// =============================================================================
+}
+// =============================================================================
